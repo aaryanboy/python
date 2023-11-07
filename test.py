4ea8025fddadc9c05f35255d5063a5b0fea05b3d
aaryanboy <aaryanboy12@gmail.com>
2023-11-07 19:53:16 +0545

    simple game

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('hello world')
+import pygame
+WIN=pygame.display.set_mode((300,300))
+
+def main():
+    run = True
+    while run:
+        print("Loop is running")  # Add this line for debugging
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False   
+        WIN.fill((60,30,30))
+        pygame.display.update()
+         
+if __name__ == "__main__":
+    pygame.init()
+    main()
+    pygame.quit()
