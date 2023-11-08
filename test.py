e92bc02a1bcbdc446d3ec2c86743660ce0212a85
aaryanboy <aaryanboy12@gmail.com>
2023-11-08 14:04:59 +0545

    learning pygame

--- test.py
+++ test.py
@@ -1,17 +1,69 @@
 import pygame
-WIN=pygame.display.set_mode((300,300))
+import sys
+import time
 
-def main():
-    run = True
-    while run:
-        print("Loop is running")  # Add this line for debugging
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False   
-        WIN.fill((60,30,30))
-        pygame.display.update()
-         
-if __name__ == "__main__":
-    pygame.init()
-    main()
-    pygame.quit()
+# Initialize pygame
+pygame.init()
+
+# Window settings
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shooting Object")
+
+# Load the sound effect
+shoot_sound = pygame.mixer.Sound("shoot.mp3")  # Replace with the actual sound file
+
+
+# Load images and set dimensions
+object_image = pygame.transform.scale(pygame.image.load("object.png"), (50, 50))
+background_image = pygame.transform.scale(pygame.image.load("space.png"), (WIDTH, HEIGHT))
+projectile_image = pygame.transform.scale(pygame.image.load("projectile.png"), (20, 20))
+
+# Object and projectile settings
+object_x, object_y = (WIDTH - 50) // 2, (HEIGHT - 50) // 2
+object_speed = 0.5
+projectiles = []
+projectile_speed = 0.5
+projectile_cooldown = 0.5
+last_shoot_time = 0
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    new_x, new_y = object_x, object_y
+
+    if keys[pygame.K_LEFT]: new_x -= object_speed
+    if keys[pygame.K_RIGHT]: new_x += object_speed
+    if keys[pygame.K_UP]: new_y -= object_speed
+    if keys[pygame.K_DOWN]: new_y += object_speed
+
+    object_x, object_y = [min(max(0, new_x), WIDTH - 50), min(max(0, new_y), HEIGHT - 50)]
+
+    current_time = time.time()
+    if keys[pygame.K_SPACE] and (current_time - last_shoot_time) >= projectile_cooldown:
+        projectiles.append((object_x + (50 - 20) / 2, object_y))
+        last_shoot_time = current_time
+         # Play the shooting sound
+        shoot_sound.play()
+
+
+    win.fill((0, 0, 0))
+    win.blit(background_image, (0, 0))
+    win.blit(object_image, (object_x, object_y))
+
+    new_projectiles = [(px, py - projectile_speed) for px, py in projectiles if py > 0]
+    projectiles = new_projectiles
+
+    for px, py in projectiles:
+        win.blit(projectile_image, (px, py))
+
+    pygame.display.update()
+
+# Quit pygame
+pygame.quit()
+sys.exit()
