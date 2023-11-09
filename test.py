98c944d002ce278d9511a2d00b8c1c0c1eff2d1a
aaryanboy <aaryanboy12@gmail.com>
2023-11-09 08:48:14 +0545

    someething guess

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ pygame.display.set_caption("Shooting Object")
 # Load the sound effect
 shoot_sound = pygame.mixer.Sound("shoot.mp3")  # Replace with the actual sound file
 
-
 # Load images and set dimensions
 object_image = pygame.transform.scale(pygame.image.load("object.png"), (50, 50))
 background_image = pygame.transform.scale(pygame.image.load("space.png"), (WIDTH, HEIGHT))
@@ -21,11 +20,15 @@ projectile_image = pygame.transform.scale(pygame.image.load("projectile.png"), (
 
 # Object and projectile settings
 object_x, object_y = (WIDTH - 50) // 2, (HEIGHT - 50) // 2
-object_speed = 0.5
+object_speed = 5
 projectiles = []
-projectile_speed = 0.5
+projectile_speed = 5
 projectile_cooldown = 0.5
 last_shoot_time = 0
+
+# Set the desired frame rate (30 FPS)
+FPS = 30
+clock = pygame.time.Clock()
 
 # Main game loop
 running = True
@@ -48,9 +51,8 @@ while running:
     if keys[pygame.K_SPACE] and (current_time - last_shoot_time) >= projectile_cooldown:
         projectiles.append((object_x + (50 - 20) / 2, object_y))
         last_shoot_time = current_time
-         # Play the shooting sound
+        # Play the shooting sound
         shoot_sound.play()
-
 
     win.fill((0, 0, 0))
     win.blit(background_image, (0, 0))
@@ -63,6 +65,9 @@ while running:
         win.blit(projectile_image, (px, py))
 
     pygame.display.update()
+
+    # Control the frame rate
+    clock.tick(FPS)
 
 # Quit pygame
 pygame.quit()
